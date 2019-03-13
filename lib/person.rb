26141b9dc1e9ff5ba583bbedb7a4bd1e2224f3b9
Riccardo <riccardo_london@live.it>
2019-03-13 08:59:50 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,20 @@
+
+    class Person
+
+      def name
+        @name
+      end
+
+      def job
+        @job
+      end
+
+      def name=(new_name)
+        @name = new_name
+      end
+
+     def job=(new_job)
+       @breed = new_job
+    end
+
+  end
